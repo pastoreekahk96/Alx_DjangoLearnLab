f28c643506f421b41a21d9a95f68b73d4a85202b
pastoreekahk96 <apastoreekahk96@gmail.com>
2025-07-15 14:42:05 +0000

    Update settings.py and README.md with project details

--- LibraryProject/LibraryProject/settings.py
+++ LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k(wvdo4!*ub2nchzddgk@6l!=tnqvrwship$0t@q5=#4r&&$7j'
+SECRET_KEY = 'n&my-@5&%ir9@t$)f$r*t647y6sz@(60^vviuot6@p#+zggq9)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
