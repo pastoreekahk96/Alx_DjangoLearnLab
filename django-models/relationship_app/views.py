8a6999aafdcc237e5539adc549143ee12fb55d55
pastoreekahk96 <apastoreekahk96@gmail.com>
2025-08-07 13:27:59 +0000

    Implement advanced model relationships, views, and URL routing in relationship_app

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,10 +5,9 @@ from .models import Library
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
-
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
