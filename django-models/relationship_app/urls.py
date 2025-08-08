c9ba907a74bc8e6b4ec621c6f17a1093e35ff11f
pastoreekahk96 <apastoreekahk96@gmail.com>
2025-08-08 18:26:48 +0000

    Add custom permissions to Book model and protect book views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('admin-role/', admin_view, name='admin_view'),
     path('librarian-role/', librarian_view, name='librarian_view'),
     path('member-role/', member_view, name='member_view'),
-    path('books/add/', add_book, name='add_book'),
-    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
 ]
 
