3d87deb0324ae44f6d7a65a5ef03fbca189db9a2
Pastoreekahk S Arthur <139224166+pastoreekahk96@users.noreply.github.com>
2026-09-03 21:56:03 +0000

    Correct Django template context processor

--- LibraryProject/LibraryProject/settings.py
+++ LibraryProject/LibraryProject/settings.py
@@ -52,7 +52,7 @@ TEMPLATES = [
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
-                "django.contrib.messages.context_processors.auth",
+                "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
         },
